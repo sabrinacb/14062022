0d049ced7d79b9949006717bf2c675df08fd0e01
sabrinacb <79062119+sabrinacb@users.noreply.github.com>
2022-06-14 12:07:43 -0300

    Add files via upload

---
+++ Formularios/codigo.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$paisVariable = $_POST['pais'];
+/*esto tiene que ser el name del menú en el html*/
+$estadoCivil  = $_POST['estado'];
+
+echo 'Hola tu pais es '.$paisVariable. ' y tu estado civil es '.$estadoCivil;
+
+
+?>
---
+++ Interfaz Web/php/codigo.php
@@ -0,0 +1,9 @@
+<?php
+
+$nombre = $_POST['nombre'];
+echo '<p style="color:blue;">Hola yo sé que tu nombre es '.$nombre.'</p>';
+
+//echo "¡Hola como estás!";
+
+
+?>
